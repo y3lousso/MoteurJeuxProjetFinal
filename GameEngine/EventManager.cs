a4de7d7008957d80d066719e7b2c5713fae26d8c
thomas <thomas.faget@etu.enseeiht.fr>
2017-12-01 18:07:50 -0500

    Update architecture of EventManager

--- GameEngine/EventManager.cs
+++ GameEngine/EventManager.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using MoteurJeuxProjetFinal.GameEngine;
 
 namespace MoteurJeuxProjetFinal.GameEngine
 {
@@ -7,14 +8,16 @@ namespace MoteurJeuxProjetFinal.GameEngine
     /// </summary>
     class EventManager
     {
-        
-        private GameEngine _gameEngine;
-        
+
+        private EventDispatcher _eventDispatcher;
+        private ListenerInfo _listenerInfo;
         private List<IEvent> _events = new List<IEvent>();
 
-        public void Init(GameEngine _gameEngine)
+        public void Init(GameEngine gameEngine)
         {
-            this._gameEngine = _gameEngine;
+            _listenerInfo = new ListenerInfo();
+            _eventDispatcher = new EventDispatcher(_listenerInfo);
+            
         }
 
         public void AddEvent(IEvent gameEvent)
@@ -32,6 +35,71 @@ namespace MoteurJeuxProjetFinal.GameEngine
             _events = new List<IEvent>();
         }
 
-        
+        public void RegisterListener(IListener listener)
+        {
+            //TODO
+        }
+
+        public EventDispatcher GetEventDispatcher()
+        {
+            return _eventDispatcher;
+        }
     }
 }
+
+    /// <summary>
+    /// A class to contains all the listeners
+    /// </summary>
+    internal class ListenerInfo
+    {
+        internal List<OnGameStartListener> OnGameStartListeners = new List<OnGameStartListener>();
+        internal List<OnGameFinishListener> OnGameFinishListeners = new List<OnGameFinishListener>();
+        internal List<OnCollisionListener> OnCollisionListeners = new List<OnCollisionListener>();
+        internal List<OnSceneChangeListener> OnSceneChangeListeners = new List<OnSceneChangeListener>();
+    }
+
+    /// <summary>
+    /// EventDispatcher : add a method too handle a new event / listener
+    /// </summary>
+    internal class EventDispatcher : IEventDispatcher
+    {
+        private ListenerInfo _listenerInfo;
+
+        internal EventDispatcher(ListenerInfo listenerInfo)
+        {
+            _listenerInfo = listenerInfo;
+        }
+        
+        public void Dispatch(GameStartEvent gameEvent)
+        {
+            foreach (OnGameStartListener listener in _listenerInfo.OnGameStartListeners)
+            {
+                listener.OnGameStart(gameEvent);
+            }
+        }
+
+        public void Dispatch(GameFinishEvent gameEvent)
+        {
+            foreach (OnGameFinishListener listener in _listenerInfo.OnGameFinishListeners)
+            {
+                listener.OnGameFinish(gameEvent);
+            }
+        }
+
+        public void Dispatch(CollisionEvent gameEvent)
+        { 
+            foreach (OnCollisionListener listener in _listenerInfo.OnCollisionListeners)
+            {
+                listener.OnCollision(gameEvent);
+            }
+        }
+
+        public void Dispatch(SceneChangeEvent gameEvent)
+        {
+            foreach (OnSceneChangeListener listener in _listenerInfo.OnSceneChangeListeners)
+            {
+                listener.OnSceneChange(gameEvent);
+            }
+        }
+
+    }
--- GameEngine/Systems/EventSystem.cs
+++ GameEngine/Systems/EventSystem.cs
@@ -6,14 +6,12 @@ namespace MoteurJeuxProjetFinal.GameEngine.Systems
     {
         private GameEngine _gameEngine;
 
-        private List<IListener> _listeners;
         private EventDispatcher _dispatcher;
         
         public void Start(GameEngine gameEngine)
         {
             _gameEngine = gameEngine;
-            _listeners= new List<IListener>();
-            _dispatcher = new EventDispatcher(_listeners);
+            _dispatcher = gameEngine.GetEventManager().GetEventDispatcher();
         }
 
         public void Update(float deltaTime)
@@ -34,51 +32,4 @@ namespace MoteurJeuxProjetFinal.GameEngine.Systems
         }
     }
 
-
-    /// <summary>
-    /// EventDispatcher : add a method too handle a new event / listener
-    /// </summary>
-    class EventDispatcher : IEventDispatcher
-    {
-        private List<IListener> _listeners;
-
-        internal EventDispatcher(List<IListener> listeners)
-        {
-            _listeners = listeners;
-        }
-        
-        public void Dispatch(GameStartEvent gameEvent)
-        {
-            foreach (OnGameStartListener listener in _listeners.FindAll(listener => listener is OnGameStartListener))
-            {
-                listener.OnGameStart(gameEvent);
-            }
-        }
-
-        public void Dispatch(GameFinishEvent gameEvent)
-        {
-            foreach (OnGameFinishListener listener in _listeners.FindAll(listener => listener is OnGameFinishListener))
-            {
-                listener.OnGameFinish(gameEvent);
-            }
-        }
-
-        public void Dispatch(CollisionEvent gameEvent)
-        { 
-            foreach (OnCollisionListener listener in _listeners.FindAll(listener => listener is OnCollisionListener))
-            {
-                listener.OnCollision(gameEvent);
-            }
-        }
-
-        public void Dispatch(SceneChangeEvent gameEvent)
-        {
-            foreach (OnSceneChangeListener listener in _listeners.FindAll(listener => listener is OnSceneChangeListener))
-            {
-                listener.OnSceneChange(gameEvent);
-            }
-        }
-
-        
-    }
 }
