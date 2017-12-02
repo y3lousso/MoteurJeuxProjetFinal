71f83a4f488204ed6d8fc7f62fbb9fd1f39525a6
thomas <thomas.faget@etu.enseeiht.fr>
2017-12-02 15:22:40 -0500

    Add script Manager (first version)

---
+++ GameEngine/GameScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace MoteurJeuxProjetFinal.GameEngine
+{
+       
+    abstract class GameScript
+    {        
+        /// <summary>
+        /// Method called by the ScriptManager to Load the script
+        /// </summary>
+        protected internal abstract void Load();
+        
+        /// <summary>
+        /// Get All the listener to register in the EventManager
+        /// </summary>
+         internal abstract List<IListener> GetListenersToRegister();
+    }
+}
---
+++ GameEngine/ScriptManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+
+namespace MoteurJeuxProjetFinal.GameEngine
+{
+    class ScriptManager
+    {
+        private List<GameScript> scripts = new List<GameScript>();
+        private GameEngine _gameEngine;
+        
+       
+        private static ScriptManager INSTANCE;
+        public ScriptManager() {}
+        public static ScriptManager GetInstance() => INSTANCE ?? (INSTANCE = new ScriptManager());
+
+        public void Init(GameEngine gameEngine)
+        {
+            _gameEngine = gameEngine;
+        }
+
+        public void AddScript(GameScript script)
+        {
+            scripts.Add(script);
+        }
+
+        /// <summary>
+        /// Call the Load mehod of all the script
+        /// </summary>
+        public void LoadAllScript()
+        {
+            foreach (GameScript script in scripts)
+            {
+                script.Load();
+            }
+        }
+        /// <summary>
+        /// register all the listener of the script
+        /// </summary>
+        public void RegisterAllScriptListener()
+        {
+            foreach (GameScript script in scripts)
+            {
+                foreach (IListener listener in script.GetListenersToRegister())
+                {
+                    _gameEngine.GetEventManager().RegisterListener(listener);
+                }
+            }
+        }
+    }
+}
