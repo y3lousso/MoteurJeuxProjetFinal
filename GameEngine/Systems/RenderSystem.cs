59eb38ae39ec63681253ef4765cfc98609ee9b90
thomas <thomas.faget@hotmail.fr>
2017-12-13 19:53:49 -0500

    Optimize render process (2nd try commit)

--- GameEngine/Systems/RenderSystem.cs
+++ GameEngine/Systems/RenderSystem.cs
@@ -1,6 +1,5 @@
-﻿﻿using System;
+﻿﻿﻿using System;
 using System.Collections.Generic;
-using System.Numerics;
 using System.Runtime.InteropServices;
 using MoteurJeuxProjetFinal.GameEngine.Components;
 
